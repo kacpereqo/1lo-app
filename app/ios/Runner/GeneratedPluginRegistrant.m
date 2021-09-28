62c15a784581d52654250481d9267f66f1da2750
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2021-09-28 19:01:40 +0200

    Update GeneratedPluginRegistrant.m

--- app/ios/Runner/GeneratedPluginRegistrant.m
+++ app/ios/Runner/GeneratedPluginRegistrant.m
@@ -1,14 +1,5 @@
-//
-//  Generated file. Do not edit.
-//
-
-// clang-format off
-
 #import "GeneratedPluginRegistrant.h"
-
 @implementation GeneratedPluginRegistrant
-
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
 }
-
 @end
